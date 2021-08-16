f4b8589f2f8d6e568a084e4ce3899954185dfaae
kooshamodabbernia <k.modabbernia@gmail.com>
2021-08-17 02:09:35 +0430

    click on not now button in notification dialog if it pops up

--- bs_test.py
+++ bs_test.py
@@ -1,17 +1,23 @@
 from selenium import webdriver
 import time
 from bs4 import BeautifulSoup
+from selenium.common.exceptions import NoSuchElementException
 
-
+CONFIG = {'username': "tarikhsazan", "passowrd": "koosha123", "instagram_profile": "kingbash"}
 if __name__ == "__main__":
-    username = "tarikhsazan"
-    password = "koosha123"
 
-    getdriver = ("https://www.instagram.com/accounts/login/")
-
+    url = "https://www.instagram.com/accounts/login/"
     driver = webdriver.Chrome()
-    driver.get(getdriver)
+    driver.get(url)
 
-    driver.find_element_by_xpath("//input[@name='username']").send_keys(username)
-    driver.find_element_by_xpath("//input[@name='password']").send_keys(password)
+    driver.find_element_by_xpath("//input[@name='username']").send_keys(CONFIG['username'])
+    driver.find_element_by_xpath("//input[@name='password']").send_keys(CONFIG['password'])
     driver.find_element_by_xpath("//button[@type='submit']").click()
+    time.sleep(5)
+
+    # notification dialog
+    try:
+        driver.find_element_by_xpath("//div[@role='dialog']")
+        driver.find_element_by_class_name('HoLwm').click()
+    except NoSuchElementException:
+        print('No notification  check')
